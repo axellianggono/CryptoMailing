d07eea742e0c751259d3c0b80425df35165a6028
Axel <93802093+jeoxn@users.noreply.github.com>
2025-11-30 14:10:06 +0700

    [UPDATE] Updating gitignore rule and SQL query

--- database.sql
+++ database.sql
@@ -1,103 +1,30 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Nov 25, 2025 at 02:20 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Buat database
+CREATE DATABASE IF NOT EXISTS `kriptowijaya`;
+USE `kriptowijaya`;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `kriptowijaya`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `mails`
---
-
-CREATE TABLE `mails` (
-  `id` int(11) NOT NULL,
-  `sender_id` int(11) NOT NULL DEFAULT 0,
-  `receiver_id` int(11) NOT NULL DEFAULT 0,
-  `message` text NOT NULL,
-  `session_key` text NOT NULL,
-  `attachment` longtext DEFAULT NULL,
-  `signature` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+-- Tabel users
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `public_key` text NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_at` datetime DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `mails`
---
-ALTER TABLE `mails`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_sender_mail` (`sender_id`),
-  ADD KEY `fk_receiver_mail` (`receiver_id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `mails`
---
-ALTER TABLE `mails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `mails`
---
-ALTER TABLE `mails`
-  ADD CONSTRAINT `fk_receiver_mail` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `fk_sender_mail` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Tabel mails
+CREATE TABLE IF NOT EXISTS `mails` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sender_id` int NOT NULL DEFAULT '0',
+  `receiver_id` int NOT NULL DEFAULT '0',
+  `message` text NOT NULL,
+  `session_key` text NOT NULL,
+  `attachment` longtext,
+  `signature` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_sender_mail` (`sender_id`),
+  KEY `fk_receiver_mail` (`receiver_id`),
+  CONSTRAINT `fk_receiver_mail` FOREIGN KEY (`receiver_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_sender_mail` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
